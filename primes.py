f62d6a6e1240cf4ae5eae6a92aca32b38d671edd
Tlthon <82746063+Tlthon@users.noreply.github.com>
2022-10-13 11:21:32 +0000

    adding prime number

--- primes.py
+++ primes.py
@@ -1,6 +1,22 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def primes(number_of_primes):
-    list = []
-    return list
+def primes(number_of_primes : int):
+    # validity check
+    if number_of_primes < 1 :
+        raise ValueError("number of primes should be more than or equal to one") 
+    list_of_primes = [2]
+    number = 3
+    while(len(list_of_primes) < number_of_primes) :
+        if isPrime(number, list_of_primes) :
+            list_of_primes.append(number)   
+
+        number += 1
+    return list_of_primes
+
+def isPrime(number : int, prime_list: list[int]) -> bool:
+    for prime in prime_list:
+        if number % prime == 0:
+            return False
+        elif prime * prime >= number:
+            return True
